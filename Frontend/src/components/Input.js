4b2cc7ad8c9283fc5286bdcf137c051f6719baeb
Elad <63946625+eladmines@users.noreply.github.com>
2024-09-23 22:36:43 +0300

    Implement 'Button', 'Image', 'Input' and 'Label' components.

---
+++ Frontend/src/components/Input.js
@@ -0,0 +1,5 @@
+export function Input({className,type,name,value,id,placholder}){
+    return(
+    <input className={className} type={type} name={name} value={value} id={id} placeholder={placholder} />
+    )
+}
