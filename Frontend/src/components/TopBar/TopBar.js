2c1f35191bd8873598ac7ddbb62f63332be67d85
Elad <63946625+eladmines@users.noreply.github.com>
2024-09-29 20:40:58 +0300

    Refactor 'TopBar' component.

--- Frontend/src/components/TopBar/TopBar.js
+++ Frontend/src/components/TopBar/TopBar.js
@@ -1,13 +1,10 @@
 import {Logo} from '../Logo'
-import {SearchBar} from '../inputs/SearchBar'
 import {NavBar} from './NavBar'
 export function TopBar(){
     return (  
     <header id="header" className="header fixed-top d-flex align-items-center">
       <Logo />
-      <SearchBar />
       <NavBar />
-    
       </header>
       );
 }
