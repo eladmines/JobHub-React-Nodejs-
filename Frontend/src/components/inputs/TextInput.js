9420838d35a7b07a5cff383de4770216b0ca9f09
Elad <63946625+eladmines@users.noreply.github.com>
2024-09-30 14:37:17 +0300

    Implement a (generic) 'TextInput' component.

---
+++ Frontend/src/components/inputs/TextInput.js
@@ -0,0 +1,23 @@
+import React, { Fragment, useState } from 'react';
+import { InputGroup, Form } from 'react-bootstrap';
+
+export function TextInput({name,placeholder,handleChildren}) {
+  const handleChange = (event) => {
+    handleChildren(name,[event.target.value]);
+};
+  return (
+    <Fragment>
+      <InputGroup hasValidation>
+        <Form.Control
+          type="text"
+          name={name}
+          placeholder={placeholder}
+          onChange={handleChange}
+        />
+        <Form.Control.Feedback type="invalid">
+          
+        </Form.Control.Feedback>
+      </InputGroup>
+    </Fragment>
+  );
+}
