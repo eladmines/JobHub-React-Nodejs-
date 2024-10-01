6aade22dbb2c9bc687bebeb35a3c97676456e8d7
Elad <63946625+eladmines@users.noreply.github.com>
2024-10-01 10:02:06 +0300

    Implement Inputs components.

---
+++ Frontend/src/components/inputs/MultiChoiceInput.js
@@ -0,0 +1,45 @@
+import { useState,useEffect } from 'react';
+import { Form, InputGroup, Row, Col } from 'react-bootstrap';
+
+export function MultiChoiceInput({handleChildren,choices,name}) {
+  const [selectedcheckboxes, setSelectedcheckboxes] = useState([]);
+
+  const handleCheckboxChange = (event) => {
+    const { value, checked } = event.target;
+
+    if (checked) {
+      setSelectedcheckboxes((prevcheckboxes) => [...prevcheckboxes, value]);
+      
+    } else {
+      setSelectedcheckboxes((prevcheckboxes) =>
+        prevcheckboxes.filter((tech) => tech !== value)
+      );
+      
+    }
+    
+  };
+
+  useEffect(() => {
+    handleChildren({name}, selectedcheckboxes);
+  }, [selectedcheckboxes]);
+
+  return (
+   
+      <InputGroup>
+        <Row>
+          {choices.map((tech, index) => (
+            <Col key={tech} xs={12} md={4} style={{ marginBottom: '10px' }}>
+              <Form.Check
+                type="checkbox"
+                id={tech}
+                label={tech}
+                value={tech}
+                onChange={handleCheckboxChange}
+              />
+            </Col>
+          ))}
+        </Row>
+      </InputGroup>
+    
+  );
+}
