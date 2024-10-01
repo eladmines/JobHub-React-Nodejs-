201e8a4791d52e8cbcea217e17e64de69d8f3ec5
Elad <63946625+eladmines@users.noreply.github.com>
2024-10-01 18:07:00 +0300

    Implement password hashing during user registration.

--- Backend/models/userModel.js
+++ Backend/models/userModel.js
@@ -1,6 +1,10 @@
 const sequelize = require('../config/db');
+const bcrypt = require('bcrypt');
+
 
 async function createUser(userData) {
+
+  const hashedPassword = await bcrypt.hash(userData['password'], 10);
   const query = `
     INSERT INTO users (firstName, lastName, email, password, experience, role, company, skills)
     VALUES ('', '', $1, $2, $3, $4, $5, $6::text[])
@@ -9,7 +13,7 @@ async function createUser(userData) {
 
   const values = [
     userData['username'],   
-    userData['password'],    
+    hashedPassword,    
     userData['experience'],  
     userData['role'],       
     userData['company'],    
