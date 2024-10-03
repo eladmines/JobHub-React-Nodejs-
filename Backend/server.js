ff451126229da1c670c486f52a11b68c9ab9f237
Elad <63946625+eladmines@users.noreply.github.com>
2024-10-03 17:27:11 +0300

    Upload routes.

--- Backend/server.js
+++ Backend/server.js
@@ -1,18 +1,25 @@
-// app.js
 const express = require('express');
 const cors = require('cors');
+const cookieParser = require("cookie-parser");
 const jobsRoute = require('./routes/jobsRoute');
 const userRoute = require('./routes/userRoute');
+const authRoute = require('./routes/authRoute');
+const jwtRoute = require('./routes/jwtRoute');
 const app = express();
-app.use(cors()); // Enable CORS
-// Middleware to parse incoming JSON requests
+app.use(cors({
+  origin: 'http://localhost:3000',
+  methods: ['GET', 'POST'],
+  credentials: true
+}));
+
 app.use(express.json());
 
-// Use the user routes
+app.use(cookieParser());
 app.use('/', jobsRoute);
 app.use('/', userRoute);
+app.use('/', authRoute);
+app.use('/', jwtRoute);
 
-// Start the server
 const PORT =  5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
