acc44f2e72fa2872e520b40dbdcd6a9880092a8b
Elad <63946625+eladmines@users.noreply.github.com>
2024-10-08 21:59:02 +0300

    Refactor routes to improve readability, scalability, and maintainability.

---
+++ Server/routes/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+
+const jobsRoute = require('./jobsRoute');
+const userRoute = require('./userRoute');
+const authRoute = require('./authRoute');
+const logoutRoute = require('./logout');
+const mainRoute = require('./mainRoute');
+const topBarRoute = require('./topBarRoute');
+const applicationsRoute = require('./applicationsRoute');
+
+// Mount routes under their respective base paths
+router.use('/jobs', jobsRoute);
+router.use('/user', userRoute);
+router.use('/auth', authRoute);
+router.use('/logout', logoutRoute);
+router.use('/main', mainRoute);
+router.use('/topbar', topBarRoute);
+router.use('/applications', applicationsRoute);
+
+module.exports = router;
