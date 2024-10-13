0057dbfb5603f0a0b8a29699559f193b327d3a4c
Elad <63946625+eladmines@users.noreply.github.com>
2024-10-13 20:09:15 +0300

    Update db details.

--- Server/config/db.js
+++ Server/config/db.js
@@ -1,14 +1,14 @@
 const { Client } = require('pg');
 
 const client = new Client({
-    database: "***", 
-    user: "***",        
-    password: "***",       
-    host: "****",
-    port: "*****",               
+    database: process.env.POSTGRES_DB,       // The database name from the environment variable
+    user: process.env.POSTGRES_USER,         // The user from the environment variable
+    password: process.env.POSTGRES_PASSWORD, // The password from the environment variable
+    host: process.env.POSTGRES_HOST,         // The host from the environment variable
+    port: process.env.POSTGRES_PORT,         // The port from the environment variable
     ssl: {
-        require: true,
-        rejectUnauthorized: false,
+        require: true,                       // SSL is required
+        rejectUnauthorized: false,           // Allow self-signed certificates (use with caution in production)
     },
 });
 
